483361885528c10101a511f86e2f8ce6f61efac6
Ashley Evans <ashley@nosegraze.com>
2017-02-26 11:26:07 +0000

    Update export to use new row filter.

--- custom-member-export-columns.php
+++ custom-member-export-columns.php
@@ -1,8 +1,17 @@
 <?php
-
-/*
+/**
+ * Adds custom user meta data to each member's export row.
+ *
  * 1. Add your column IDs / Names
  * 2. Fill the column values from get_user_meta() using your unique meta keys
+ */
+
+/**
+ * Add additional columns
+ *
+ * @param array $columns Array of columns.
+ *
+ * @return array
  */
 function pw_rcp_register_export_columns( $columns ) {
 	$columns[ 'my_custom_column' ] = 'My Column Name';
@@ -11,16 +20,20 @@ function pw_rcp_register_export_columns( $columns ) {
 }
 add_filter( 'rcp_export_csv_cols_members', 'pw_rcp_register_export_columns' );
 
-function pw_rcp_add_fields_to_export( $data ) {
+/**
+ * Add values to each member's row
+ *
+ * @param array      $data   Array of member data.
+ * @param RCP_Member $member RCP member object.
+ *
+ * @return array Array with new data added.
+ */
+function pw_rcp_add_fields_to_export( $data, $member ) {
 	
-	foreach( $data as $key => $row ) {
-
-		$data[ $key ][ 'my_custom_column' ] = get_user_meta( $row['user_id'], 'my_meta_key', true );
-		$data[ $key ][ 'my_second_custom_column' ] = get_user_meta( $row['user_id'], 'my_second_meta_key', true );
-
-	}
+	$data[ 'my_custom_column' ] = get_user_meta( $member->ID, 'my_meta_key', true );
+	$data[ 'my_second_custom_column' ] = get_user_meta( $member->ID, 'my_second_meta_key', true );
 
 	return $data;
 
 }
-add_filter( 'rcp_export_get_data_members', 'pw_rcp_add_fields_to_export' );
+add_filter( 'rcp_export_members_get_data_row', 'pw_rcp_add_fields_to_export', 10, 2 );
