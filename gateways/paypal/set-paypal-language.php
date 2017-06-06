abef000dc974f4ca8fd2833533ff89a604525933
John Parris <public@johnparris.com>
2017-06-06 12:16:24 -0400

    Add example for setting PayPal Standard language

---
+++ gateways/paypal/set-paypal-language.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This will instruct PayPal Standard to use the
+ * specified language instead of the language
+ * set in your PayPal account.
+ *
+ * See supported locales here: https://developer.paypal.com/docs/classic/api/locale_codes/
+ */
+add_filter( 'rcp_paypal_args', function( $args ) {
+	$args['lc'] = 'es_ES'; // change es_ES to whatever you want
+	return $args;
+});
