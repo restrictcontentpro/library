8b0da38b18b8ce23924fd9466401d70ddae5dcf6
John Parris <public@johnparris.com>
2017-08-30 14:07:51 -0400

    Add body class on RCP pages. props @cklosowski

---
+++ add-body-class-for-rcp-pages.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Adds body classes to the registered pages in the Restrict Content Pro settings
+ *
+ * @param $classes
+ *
+ * @return array
+ */
+function jp_add_body_classes( $classes ) {
+	global $post, $rcp_options;
+
+	if ( ! is_object( $post ) ) {
+		return $classes;
+	}
+
+	if ( ! is_page() ) {
+		return $classes;
+	}
+
+	$page_classes = array(
+		'rcp-registration'   => isset( $rcp_options['registration_page'] ) ? $rcp_options['registration_page'] : 0,
+		'rcp-success'        => isset( $rcp_options['redirect'] )          ? $rcp_options['redirect']          : 0,
+		'rcp-account'        => isset( $rcp_options['account_page'] )      ? $rcp_options['account_page']      : 0,
+		'rcp-edit-profile'   => isset( $rcp_options['edit_profile'] )      ? $rcp_options['edit_profile']      : 0,
+		'rcp-update-billing' => isset( $rcp_options['update_card'] )       ? $rcp_options['update_card']       : 0,
+	);
+
+	$found_classes = array_keys( $page_classes, $post->ID );
+
+	if ( ! empty( $found_classes ) ) {
+		foreach ( $found_classes as $class ) {
+			$classes[] = $class;
+		}
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'jp_add_body_classes', 10, 1 );
