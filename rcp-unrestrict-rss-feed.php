45d50f28d038cf8849dd0999a422dab0aa6c0340
Ashley <ashley@nosegraze.com>
2018-08-10 10:13:01 +0100

    Add snippet for unrestricting RSS feeds.

---
+++ rcp-unrestrict-rss-feed.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Restrict Content Pro - Unrestrict RSS Feed
+ * Description: Removes post restrictions from RSS feeds. Note that RSS feeds are publicly accessible so this will make your content visible to non-members if they visit your feed URL.
+ * Version: 1.0
+ * Author: Restrict Content Pro team
+ * License: GPL2
+ */
+
+/**
+ * Remove restrictions from RSS feeds.
+ *
+ * @param string $content
+ *
+ * @return string
+ */
+function ag_rcp_remove_restrictions_feeds( $content ) {
+	if ( is_feed() ) {
+		remove_filter( 'the_content', 'rcp_filter_restricted_content' , 100 );
+	}
+
+	return $content;
+}
+
+add_filter( 'the_content', 'ag_rcp_remove_restrictions_feeds' , 10 );
