3b1592b346f21ac23731857712a059b11de7ce53
Ashley <ashley@nosegraze.com>
2019-03-29 13:27:07 +0000

    Update membership export snippet for 3.0. #39

--- rcp-custom-member-export-columns.php
+++ rcp-custom-member-export-columns.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: Restrict Content Pro - Custom Member Export Columns
- * Description: Adds custom user meta data to each member's export row.
+ * Plugin Name: Restrict Content Pro - Custom Membership Export Columns
+ * Description: Adds custom user meta or membership meta data to each membership export row.
  * Version: 1.0
  * Author: Restrict Content Pro Team
  * License: GPL2
@@ -24,24 +24,36 @@
 function pw_rcp_register_export_columns( $columns ) {
 	$columns[ 'my_custom_column' ] = 'My Column Name';
 	$columns[ 'my_second_custom_column' ] = 'My Second Column Name';
+	$columns[ 'my_third_custom_column' ] = 'My Third Column Name';
+
 	return $columns; 
 }
 add_filter( 'rcp_export_csv_cols_members', 'pw_rcp_register_export_columns' );
 
 /**
- * Add values to each member's row
+ * Add values to each membership row
  *
- * @param array      $data   Array of member data.
- * @param RCP_Member $member RCP member object.
+ * @param array          $data       Array of member data to be included in the CSV export.
+ * @param RCP_Membership $membership Membership object.
  *
  * @return array Array with new data added.
  */
-function pw_rcp_add_fields_to_export( $data, $member ) {
-	
-	$data[ 'my_custom_column' ] = get_user_meta( $member->ID, 'my_meta_key', true );
-	$data[ 'my_second_custom_column' ] = get_user_meta( $member->ID, 'my_second_meta_key', true );
+function pw_rcp_add_fields_to_export( $data, $membership ) {
+
+	/*
+	 * This example is for including user meta data.
+	 */
+	$user_id = $membership->get_customer()->get_user_id();
+
+	$data[ 'my_custom_column' ] = get_user_meta( $user_id, 'my_meta_key', true );
+	$data[ 'my_second_custom_column' ] = get_user_meta( $user_id, 'my_second_meta_key', true );
+
+	/*
+	 * This example is for including membership meta data.
+	 */
+	$data[ 'my_third_custom_column' ] = rcp_get_membership_meta( $membership->get_id(), 'my_third_meta_key', true );
 
 	return $data;
 
 }
-add_filter( 'rcp_export_members_get_data_row', 'pw_rcp_add_fields_to_export', 10, 2 );
+add_filter( 'rcp_export_memberships_get_data_row', 'pw_rcp_add_fields_to_export', 10, 2 );
