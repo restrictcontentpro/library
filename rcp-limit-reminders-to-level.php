d871e502630a14059c7e04f423b08dba927b6db8
Ashley <ashley@nosegraze.com>
2019-03-29 13:29:06 +0000

    Update reminder snippet for 3.0. #39

--- rcp-limit-reminders-to-level.php
+++ rcp-limit-reminders-to-level.php
@@ -1,27 +1,27 @@
 <?php
 /**
  * Plugin Name: Restrict Content Pro - Reminders for One Level Only
- * Description: Only send expiration/renewal reminders for one subscription level.
+ * Description: Only send expiration/renewal reminders for one membership level.
  * Version: 1.0
  * Author: Restrict Content Pro Team
  * License: GPL2
  */
 
 /**
- * Normally expiration and renewal reminders are sent to members of all subscription levels.
- * This snippet limits the emails to one specific subscription level only. There are two
+ * Normally expiration and renewal reminders are sent to members of all membership levels.
+ * This snippet limits the emails to one specific membership level only. There are two
  * functions shown below: the first limits expiration reminders to your chosen level
  * and the other limits renewal reminders to your chosen level. You may use both functions
  * or just choose one.
  */
 
 /**
- * Limits expiration reminders to your chosen subscription level only. Only users on this
- * subscription level will receive expiration reminder emails.
+ * Limits expiration reminders to your chosen membership level only. Only users on this
+ * membership level will receive expiration reminder emails.
  *
  * @param array  $args   Query arguments.
  * @param string $period Reminder period.
- * @param string $type   Type of notice to get the subscriptions for (renewal or expiration).
+ * @param string $type   Type of notice to get the memberships for (renewal or expiration).
  *
  * @return array Modified query arguments.
  */
@@ -31,10 +31,7 @@ function ag_rcp_limit_expiration_reminders_to_level( $args, $period, $type ) {
 		return $args;
 	}
 
-	$args['meta_query'][] = array(
-		'key'   => 'rcp_subscription_level',
-		'value' => 3 // ID of your subscription level here
-	);
+	$args['object_id'] = 3; // ID of your membership level here
 
 	return $args;
 
@@ -43,12 +40,12 @@ function ag_rcp_limit_expiration_reminders_to_level( $args, $period, $type ) {
 add_filter( 'rcp_reminder_subscription_args', 'ag_rcp_limit_expiration_reminders_to_level', 10, 3 );
 
 /**
- * Limits renewal reminders to your chosen subscription level only. Only users on this
- * subscription level will receive renewal reminder emails.
+ * Limits renewal reminders to your chosen membership level only. Only users on this
+ * membership level will receive renewal reminder emails.
  *
  * @param array  $args   Query arguments.
  * @param string $period Reminder period.
- * @param string $type   Type of notice to get the subscriptions for (renewal or expiration).
+ * @param string $type   Type of notice to get the memberships for (renewal or expiration).
  *
  * @return array Modified query arguments.
  */
@@ -58,10 +55,7 @@ function ag_rcp_limit_renewal_reminders_to_level( $args, $period, $type ) {
 		return $args;
 	}
 
-	$args['meta_query'][] = array(
-		'key'   => 'rcp_subscription_level',
-		'value' => 3 // ID of your subscription level here
-	);
+	$args['object_id'] = 3; // ID of your membership level here
 
 	return $args;
 
