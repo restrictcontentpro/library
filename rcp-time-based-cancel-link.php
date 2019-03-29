62a26e0d482382acb604d626cbddfeb5994d4879
Ashley <ashley@nosegraze.com>
2019-03-29 13:33:17 +0000

    Update time based cancel link snippet for 3.0. #39

--- rcp-time-based-cancel-link.php
+++ rcp-time-based-cancel-link.php
@@ -8,38 +8,43 @@
  */
 
 /**
- * Prevents the "Cancel your subscription" link from showing
- * until the member has been subscribed to his or her current
- * subscription for 3 months.
+ * Prevents the "Cancel your membership" link from showing until the membership has been
+ * active for at least 3 months.
+ *
+ * @param bool           $can_cancel    Whether or not the membership can be cancelled.
+ * @param int            $membership_id ID of the membership being checked.
+ * @param RCP_Membership $membership    Membership object.
+ *
+ * @return bool
+ * @throws Exception
  */
-function jp_rcp_member_can_cancel( $ret, $user_id ) {
+function jp_rcp_membership_can_cancel( $can_cancel, $membership_id, $membership ) {
 
 	global $rcp_options;
 
 	// Only do this on the Account Page
 	if ( empty( $rcp_options['account_page'] ) || ! is_page( $rcp_options['account_page'] ) ) {
-		return $ret;
+		return $can_cancel;
 	}
 
 	// Return early if other conditions aren't already met.
-	if ( ! $ret ) {
+	if ( ! $can_cancel ) {
 		return false;
 	}
 
 	$timezone = get_option( 'timezone_string' );
 	$timezone = ! empty( $timezone ) ? $timezone : 'UTC';
 
-	$member   = new RCP_Member( $user_id );
-
-	$cancel_date = new \DateTime( $member->get_joined_date(), new \DateTimeZone( $timezone ) );
+	$cancel_date = new \DateTime( $membership->get_created_date( false ), new \DateTimeZone( $timezone ) );
 	$cancel_date->modify( '+3 months'); // change this if you want a different time period
 
 	$now = new \DateTime( 'now', new \DateTimeZone( $timezone ) );
 
-	if ( $ret && ( $now < $cancel_date ) ) {
-		$ret = false;
+	if ( $can_cancel && ( $now < $cancel_date ) ) {
+		$can_cancel = false;
 	}
 
-	return $ret;
+	return $can_cancel;
+
 }
-add_filter( 'rcp_member_can_cancel', 'jp_rcp_member_can_cancel', 100, 2 );
+add_filter( 'rcp_membership_can_cancel', 'jp_rcp_membership_can_cancel', 100, 3 );
